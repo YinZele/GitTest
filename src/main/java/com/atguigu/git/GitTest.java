08dbc3cd0cb4116085189a681e5025f061091ead
尹泽乐 <106514316+YinZele@users.noreply.github.com>
2022-05-30 22:19:18 +0800

    master_pull

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -9,6 +9,7 @@ public class GitTest {
         System.out.println("hello git5!");
         System.out.println("hot-fix!!!");
         System.out.println("push!!!");
+        System.out.println("pull!!!");
 
     }
 }
